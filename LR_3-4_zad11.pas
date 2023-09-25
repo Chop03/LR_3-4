df09e6a50f20b1ca35a12b02a5a1a8f4639b1e14
Chop03 <144237862+Chop03@users.noreply.github.com>
2023-09-25 20:42:00 +0300

    Add files via upload

--- LR_3-4_zad11.pas
+++ LR_3-4_zad11.pas
@@ -1,11 +1,12 @@
 ﻿program zad11;
 var n,f:integer;
 begin
+  writeln('Введите число: ');
   f:=1;
     readln(n);
   while n>0 do begin
     f:=f*n;
     n:=n-1;
   end;
-  writeln(f);
+  writeln('Факториал вашего числа: ',f);
 end.
