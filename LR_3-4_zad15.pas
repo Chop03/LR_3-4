651906dac8db645df689dc725d8b645ffaec5f3a
Chop03 <144237862+Chop03@users.noreply.github.com>
2023-09-26 09:22:55 +0300

    Add files via upload

---
+++ LR_3-4_zad15.pas
@@ -0,0 +1,14 @@
+﻿program zad15;
+var n,sum,x,y : integer;
+begin
+  x:= 1;
+  writeln('Введите число: ');
+  readln(n);
+  while n mod 10 >= 1 do begin
+    sum:= sum + n mod 10;
+    x:= x*(n mod 10);
+    n:= n div 10;
+    y:= y + 1;
+  end;
+  writeln('Сумма = ',sum,', Произведение = ',x,' Количество = ', y);
+end.
